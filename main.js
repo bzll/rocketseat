4834349e73b8d92f797400834f23c47d4a773868
Ciro Chagas <ciro.chagas@agroamazonia.com>
2019-06-10 14:03:30 -0400

    Z

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-class Teste {
-    metodo(){
-        
+class List{
+    constructor() {
+        this.data = [];
     }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List {
+    constructor(){
+        super();
+
+        this.usuario = 'Ciro';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+    
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo Todo');
+    MinhaLista.mostraUsuario();
 }
