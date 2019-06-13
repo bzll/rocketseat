1b61c0cf3e49a65d859f7e2ce5672e4e5b79d34b
Ciro Chagas <ciro.chagas@agroamazonia.com>
2019-06-13 17:09:02 -0400

    starter_es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const path_public = path.resolve(__dirname,'starter','public')
+const path_public = path.resolve(__dirname,'starter_es6','public')
 module.exports = {
-    entry: ['@babel/polyfill','./starter/src/main.js'],    
+    entry: ['@babel/polyfill','./starter_es6/src/main.js'],    
     output: {
         path: path_public,
         filename: 'bundle.js',
